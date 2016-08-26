07bd850ec1fd3dc1c37a8ba5ef22784bd2908298
Pair <pair@pivotal.io>
2016-08-26 12:06:20 +0800

    Add Space as dual key Ctl and Space

--- keyboards/phantom/keymaps/default/keymap.c
+++ keyboards/phantom/keymaps/default/keymap.c
@@ -61,6 +61,9 @@ enum macro_id {
 // GUI or Backspace
 #define GUIBSPC MT(MOD_LGUI, KC_BSPC)
 
+// Ctrl or Space
+#define CTLSPC MT(MOD_LCTL, KC_SPC)
+
 // Numpad short cuts
 #define N_PLUS KC_KP_PLUS
 #define N_SLSH KC_KP_SLASH
@@ -91,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           ALTTAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, ALTBSLS,        KC_DEL,  KC_END, KC_PGDN, \
          CTLBSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,          CTLENTR, \
          KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                   KC_RSFT,                  KC_UP, \
-         KC_LCTL, KC_LALT, KC_LGUI,                    KC_SPC,                                     KC_RGUI, KC_RALT,   MO(1), KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT),
+         KC_LCTL, KC_LALT, KC_LGUI,                    CTLSPC,                                     KC_RGUI, KC_RALT,   MO(1), KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Layer 1: FN0 - When FN is held down */
   /*
